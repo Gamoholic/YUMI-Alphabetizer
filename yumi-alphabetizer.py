142d256cc86d5d8d0313f2971819ff424354215e
Gamoholic <github.com@gamoholic.info>
2014-04-20 01:30:37 -0600

    Stole section titles from KYSU. Added a LOT of comments. Shortened the number of lines used to assign cfg_list. Updated the logic to handle a format change that I think occured in the update to YUMI 2.

--- yumi-alphabetizer.py
+++ yumi-alphabetizer.py
@@ -1,48 +1,75 @@
 #!/usr/bin/env python
 import sys, re, os
 
+#------------------------------------------------------------------------------#
+# Global Variables                                                             #
+#------------------------------------------------------------------------------#
 
+ARGS = sys.argv[1:]
+if not ARGS:
+    print 'usage: mountpoint_of_drive'
+    sys.exit(1)
+
+
+
+#------------------------------------------------------------------------------#
+# Functions                                                                    #
+#------------------------------------------------------------------------------#
 
 def alpha(file_in):
     file_open = open(file_in, 'rU').read()
-    file_open = re.sub(' \n', '\n', file_open)
-    file_open = re.sub('\n{3,}', '\n\n', file_open)
-    file_split = file_open.split('\nlabel')
-    file_dict = {}
     
+    # Remove spaces before newlines
+    file_open = re.sub(' \n', '\n', file_open)
+    
+    # Remove excess newlines
+    file_open = re.sub('\n{3,}', '\n\n', file_open)
+    
+    # Set the word used to split the config into its menu blocks
     if file_in.endswith('syslinux.cfg'):
-        final = file_split[0] + '\n' + 'label' + file_split[1]
-        entries_to_sort = file_split[2:]
+        split_word = 'label'
     else:
-        final = file_split[0]
-        entries_to_sort = file_split[1:]
+        split_word = '#start'
+    
+    # Split the config into its menu blocks
+    file_split = file_open.split('\n' + split_word)
+    
+    # Divide the config so the first block doesn't get sorted
+    final = file_split[0]
+    entries_to_sort = file_split[1:]
         
+    file_dict = {}    
     for entry in entries_to_sort:
-        entry = 'label' + entry
-        entry_name = re.search('label (.*?)\n', entry).group(1)
+        
+        # The split function removes the split_word, so add it back
+        entry = split_word + entry
+        
+        # Find the name of the block and add it the dictionary
+        entry_name = re.search(split_word + ' (.*?)\n', entry).group(1)
         file_dict[entry_name] = entry
         
+    # All this work just to get ready for these 2 lines...
     for key in sorted(file_dict):
         final += '\n' + file_dict[key]
         
+    # Write the sorted config
     file_out = open(file_in, 'w')
     file_out.write(final)
     file_out.close
 
 
 
-# Main
-rootdir = sys.argv[1]
+#------------------------------------------------------------------------------#
+# Main                                                                         #
+#------------------------------------------------------------------------------#
+
+rootdir = ARGS[0]
 sub_dir = os.path.join(rootdir, 'multiboot')
 main_dir = os.path.join(sub_dir, 'menu')
 local_list = os.listdir(main_dir)
 cfg_list = [
-    'antivirus.cfg',
-    'linux.cfg',
-    'netbook.cfg',
-    'other.cfg',
-    'system.cfg',
-    'isos.cfg']
+    'antivirus.cfg', 'linux.cfg', 'netbook.cfg',
+    'other.cfg', 'system.cfg', 'isos.cfg']
 
 for config in cfg_list:
     if config in local_list:
